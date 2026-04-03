0419d7dd87de785bc47af2eb2c60db01724da88b
MataVsn <113342160+MataVsn@users.noreply.github.com>
2026-04-03 22:01:30 +0300

    Comp - system

--- Content.Shared/_Sunrise/Abilities/BorgEnvironmentMonitorComponent.cs
+++ Content.Shared/_Sunrise/Abilities/BorgEnvironmentMonitorComponent.cs
@@ -6,18 +6,18 @@ namespace Content.Shared._Sunrise.Abilities;
 [RegisterComponent]
 public sealed class BorgEnvironmentMonitorComponent : Component
 {
-    [DataField("range")]
-    public float Range = 10f;
+    [DataField] public TimeSpan UpdateInterval = TimeSpan.FromSeconds(1);
 
-    [DataField("updateInterval")]
-    public TimeSpan UpdateInterval = TimeSpan.FromSeconds(1);
+    [DataField] public float MinPressure = 50f;
+    [DataField] public float MaxPressure = 300f;
 
-    [DataField]
-    public ProtoId<AlertPrototype> HighPressureAlert = "HighPressure";
+    [DataField] public float MinTemperature = 260f;
+    [DataField] public float MaxTemperature = 360f;
+    [DataField] public float MinOxygenPartialPressure = 16f;
 
-    [DataField]
-    public ProtoId<AlertPrototype> LowPressureAlert = "LowPressure";
+    [DataField] public ProtoId<AlertPrototype> HighPressureAlert = "HighPressure";
+    [DataField] public ProtoId<AlertPrototype> LowPressureAlert = "LowPressure";
+    [DataField] public ProtoId<AlertPrototype> TemperatureAlert = "Temperature";
+    [DataField] public ProtoId<AlertPrototype> LowOxygenAlert = "LowOxygen";
 
-    [DataField]
-    public ProtoId<AlertPrototype> TemperatureAlert = "Temperature";
 }
--- Content.Shared/_Sunrise/Abilities/BorgEnvironmentMonitorSystem.cs
+++ Content.Shared/_Sunrise/Abilities/BorgEnvironmentMonitorSystem.cs
@@ -1,32 +1,75 @@
-using Content.Shared.Actions;
-using Robust.Shared.Localization;
+using Content.Shared.Alert;
+using Content.Shared.Atmos;
+using Robust.Shared.GameObjects;
+using Robust.Shared.Timing;
 
 namespace Content.Shared._Sunrise.Abilities;
 
 public sealed class BorgEnvironmentMonitorSystem : EntitySystem
 {
-    [Dependency] private readonly SharedPopupSystem _popup = default!;
+    [Dependency] private readonly AlertsSystem _alerts = default!;
+    [Dependency] private readonly AtmosphereSystem _atmos = default!;
+    [Dependency] private readonly IGameTiming _timing = default!;
 
-    public override void Initialize()
+    public override void Update(float frameTime)
     {
-        SubscribeLocalEvent<BorgEnvironmentMonitorComponent, GetVerbsEvent<ActivationVerb>>(OnGetActivationVerbs);
+        var query = EntityQueryEnumerator<BorgEnvironmentMonitorComponent>();
+
+        while (query.MoveNext(out var uid, out var comp))
+        {
+            if (!_timing.IsFirstTimePredicted)
+                continue;
+
+            if (!TryComp<TransformComponent>(uid, out var xform))
+                continue;
+
+            if (!comp.NextUpdate.TryGetValue(uid, out var next))
+                next = TimeSpan.Zero;
+
+            if (next > _timing.CurTime)
+                continue;
+
+            comp.NextUpdate[uid] = _timing.CurTime + comp.UpdateInterval;
+
+            UpdateEnvironment(uid, comp, xform);
+        }
     }
 
-    private void OnGetActivationVerbs(EntityUid uid, BorgEnvironmentMonitorComponent component, GetVerbsEvent<ActivationVerb> args)
+    private void UpdateEnvironment(EntityUid uid, BorgEnvironmentMonitorComponent comp, TransformComponent xform)
     {
-        if (!args.CanAccess || !args.CanInteract)
+        var mixture = _atmos.GetTileMixture(xform.Coordinates);
+
+        if (mixture == null)
             return;
 
-        ActivationVerb verb = new()
-        {
-            Text = Loc.GetString("borg-env-mon-verb"),
-            Act = () => MonitorEnvironment(uid, args.User),
-        };
-        args.Verbs.Add(verb);
-    }
+        var pressure = mixture.Pressure;
+        var temp = mixture.Temperature;
 
-    private void MonitorEnvironment(EntityUid uid, EntityUid user)
-    {
-        _popup.PopupEntity(Loc.GetString("borg-env-mon-message"), uid, user);
+        // ---- Давление ----
+        var lowPressure = pressure < comp.MinPressure;
+        var highPressure = pressure > comp.MaxPressure;
+
+        _alerts.SetAlert(uid, comp.LowPressureAlert, lowPressure);
+        _alerts.SetAlert(uid, comp.HighPressureAlert, highPressure);
+
+        // ---- Температура ----
+        var badTemp = temp < comp.MinTemperature || temp > comp.MaxTemperature;
+
+        _alerts.SetAlert(uid, comp.TemperatureAlert, badTemp);
+
+        // ---- Кислород ----
+        var oxygen = mixture.GetMoles(Gas.Oxygen);
+        var totalMoles = mixture.TotalMoles;
+
+        float oxygenRatio = 0f;
+
+        if (totalMoles > 0)
+            oxygenRatio = oxygen / totalMoles;
+
+        var oxygenPartialPressure = oxygenRatio * pressure;
+
+        var lowOxygen = oxygenPartialPressure < comp.MinOxygenPartialPressure;
+
+        _alerts.SetAlert(uid, comp.LowOxygenAlert, lowOxygen);
     }
 }
